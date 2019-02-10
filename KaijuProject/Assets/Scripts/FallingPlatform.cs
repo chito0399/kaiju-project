c08eb526ad6526327299d70e990433328adee97d
chito0399 <37593552+chito0399@users.noreply.github.com>
2019-02-10 00:03:29 -0600

    BasicThings

---
+++ KaijuProject/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float fall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fall);
+        rb.isKinematic = false;
+        GetComponent<Collider2D>().isTrigger = true;
+        yield return 0;
+    }
+}
