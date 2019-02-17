53fccf26cd1d0e11a0c845923a8458a0f228e613
chito0399 <37593552+chito0399@users.noreply.github.com>
2019-02-16 21:19:12 -0600

    ObstaculosArriba

---
+++ KaijuProject/Assets/Scripts/Blocks.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+
+    private bool used;                  //To indicate if the block has already been touced;
+    public Transform newPower;         //The prefab of the new power.
+    public GameObject positionPower;   //the position where the power will come out.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        used = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (used)
+            {
+                Transform power = Instantiate(newPower, positionPower.transform.position, positionPower.transform.rotation);
+                used = !used;
+            }
+        }
+    }
+}
