2501c95e5a77ad135d4c26046f5f2e2190aad69f
chito0399 <chito99salu@outlook.com>
2019-04-04 09:15:12 -0600

    Obstáculo de compuertas
    El jugador queda atrapado con el enémigo y tiene que matarlo para poder seguir el juego

---
+++ KaijuProject/Assets/Scripts/Compuertas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compuertas : MonoBehaviour
+{
+    public GameObject izquierda;
+    public GameObject derecha;
+
+    private Vector3 start_izq;
+    private Vector3 start_der;
+
+    public EnemyManager enemy;
+
+    private Rigidbody2D rb_izq;        //RigidBody de la plataforma
+    private Rigidbody2D rb_der;        //RigidBody de la plataforma
+    
+    public float fall = 1f;       //Tiempo en responder a la caída
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_izq = izquierda.transform.position;
+        start_der = derecha.transform.position;
+
+        rb_izq = izquierda.GetComponent<Rigidbody2D>();
+        rb_der = derecha.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        int enemyHealth = enemy.gameObject.GetComponent<EnemyManager>().health;
+        if (enemyHealth <= 0)
+        {
+            rb_izq.transform.position = start_izq;
+            rb_der.transform.position = start_der;
+
+            rb_der.gravityScale = 0f;
+            rb_izq.gravityScale = 0f;
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fall);
+        }
+    }
+    
+    void Fall()
+    {
+        rb_izq.gravityScale = 98.1f;
+        rb_der.gravityScale = 98.1f;
+    }
+
+
+}
--- KaijuProject/Assets/Weapon.cs
+++ KaijuProject/Assets/Weapon.cs
@@ -27,11 +27,13 @@ public class Weapon : MonoBehaviour
         if (power2 && countShots2>=0)
         {
             Instantiate(bullet2Prefab, firePoint.position, firePoint.rotation);
+            FindObjectOfType<AudioManager>().Play("Shoot1");
             countShots2--;
         }
         else
         {
             Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            FindObjectOfType<AudioManager>().Play("Shoot1");
         }
     }
 }
