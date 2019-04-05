48d91464588168f7599879d6d8e5299fdda96e8c
chito0399 <chito99salu@outlook.com>
2019-04-04 18:24:57 -0600

    Cannon obstacle
    El obstaculo del canon esta casi listo

---
+++ KaijuProject/Assets/Scripts/Cannon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float timeDestroy; // time it delays to destroy
+    public float timeFire;    // time it delays to shoot
+
+    private bool inside;
+
+    public Transform cannonBall;
+
+    public GameObject position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Shoot()
+    {
+        if (inside)
+        {
+            //Debug.Log("fireball");
+            yield return new WaitForSeconds(timeFire);
+            Transform cannonb = Instantiate(cannonBall, position.transform.position, position.transform.rotation);
+            Destroy(cannonb.gameObject, timeDestroy);
+            StartCoroutine(Shoot());
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Canon");
+            inside = true;
+            StartCoroutine(Shoot());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inside = false;
+        StopCoroutine(Shoot());
+    }
+}
---
+++ KaijuProject/Assets/Scripts/CannonBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBullet : MonoBehaviour
+{
+    public Player player;
+    public int damage; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        gameObject.transform.Translate(new Vector3(0, 1f, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.GetDamage(damage);
+            Debug.Log("el cannon le dio al player");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+
+}
